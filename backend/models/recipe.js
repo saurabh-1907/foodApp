231b2f81a6a62ea28b4a36fbb5a1e8b9d7aeab6a
Saurabh Pandey <147328092+saurabh-1907@users.noreply.github.com>
2025-09-08 19:32:37 +0530

    Handle missing recipe data and refine recipe navigation

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -26,4 +26,4 @@ const recipeSchema=mongoose.Schema({
 
 },{timestamps:true})
 
-module.exports=mongoose.model("Recipes",recipeSchema)
+module.exports = mongoose.model("Recipes", recipeSchema)
--- frontend/food-blog-app/src/pages/RecipeDetails.jsx
+++ frontend/food-blog-app/src/pages/RecipeDetails.jsx
@@ -4,25 +4,45 @@ import food from '../assets/foodRecipe.png'
 import { useLoaderData } from 'react-router-dom'
 
 export default function RecipeDetails() {
-    const recipe=useLoaderData()
-    console.log(recipe)
-  return (
-   <>
-    <div className='outer-container'>
-        <div className='profile'>
-            <img src={profileImg} width="50px" height="50px"></img>
-            <h5>{recipe.email}</h5>
-        </div>
-        <h3 className='title'>{recipe.title}</h3>
-        {recipe.coverImage ?
-            <img src={recipe.coverImage} width="220px" height="200px" alt={recipe.title} /> :
-            <img src={food} width="220px" height="200px" alt="Default recipe image" />
-        }
-        <div className='recipe-details'>
-            <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map((item, index)=>(<li key={index}>{item}</li>))}</ul></div>
-            <div className='instructions'><h4>Instructions</h4><p>{recipe.instructions}</p></div>
-        </div>
-    </div>
-   </>
-  )
+    const recipe = useLoaderData()
+
+    if (!recipe) {
+        return (
+            <div className='outer-container'>
+                <p>Recipe not found.</p>
+            </div>
+        )
+    }
+
+    const ingredients = Array.isArray(recipe.ingredients) ? recipe.ingredients : []
+
+    return (
+        <>
+            <div className='outer-container'>
+                <div className='profile'>
+                    <img src={profileImg} width="50px" height="50px" alt="profile" />
+                    <h5>{recipe.email}</h5>
+                </div>
+                <h3 className='title'>{recipe.title}</h3>
+                {recipe.coverImage ?
+                    <img src={recipe.coverImage} width="220px" height="200px" alt={recipe.title} /> :
+                    <img src={food} width="220px" height="200px" alt="Default recipe image" />
+                }
+                <div className='recipe-details'>
+                    <div className='ingredients'>
+                        <h4>Ingredients</h4>
+                        <ul>
+                            {ingredients.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className='instructions'>
+                        <h4>Instructions</h4>
+                        <p>{recipe.instructions}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
